9f9e66577fc9791e358eac8546dac5b7f3ac4761
Marco Tinoco <marco.tinoco@medicineone.net>
2020-02-14 13:16:49 +0000

    Add project files.

---
+++ SFTPClient/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace SFTPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Process Start");
+
+
+            string[] linhas = File.ReadAllLines("settings.cfg");
+            
+            String conn = linhas[0];
+            String db = linhas[1];
+            string backupfile = linhas[2];
+            string SFTPuser = linhas[3];
+            string SFTPpass = linhas[4];
+            string SFTPhost = linhas[5];
+            string SFTPpath = linhas[6];
+
+            ProcessDB(db, conn);
+
+            string path = String.Concat(Environment.CurrentDirectory, "\\", backupfile);
+
+            CreateBackup(db, path, conn);
+
+            SFTPClient.SendFileToServer.Send(backupfile, SFTPhost, SFTPuser, SFTPpass,SFTPpath);
+        }
+
+        static void ProcessDB(string DBName,string connString)
+        {
+            String sql = Resources.Resources.ProcessDB;
+            sql = sql.Replace("@@dbname", DBName); 
+
+
+
+            ExecuteCommand(sql, connString);
+
+        }
+        static void CreateBackup(string DBName,string backupFile, string connString)
+        {
+            String sql = Resources.Resources.CreateBackup;
+            sql = sql.Replace("@@dbname", DBName).Replace("@@backupfile", backupFile);
+
+           ExecuteCommand(sql, connString);
+
+        }
+
+        static void ExecuteCommand(string sql,string connectionString)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ SFTPClient/SFTPClient.cs
@@ -0,0 +1,31 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFTPClient
+{
+    public static class SendFileToServer
+    {
+        public static int Send(string fileName,string host,string user,string pass,string remotePath)
+        {
+            var connectionInfo = new ConnectionInfo(host, user, new PasswordAuthenticationMethod(user, pass));
+            // Upload File
+            using (var sftp = new SftpClient(connectionInfo))
+            {
+
+                sftp.Connect();
+                string s = sftp.WorkingDirectory;
+                sftp.ChangeDirectory(string.Concat( @"/", remotePath));
+                //sftp.ChangeDirectory("/MyFolder");
+                using (var uplfileStream = System.IO.File.OpenRead(fileName))
+                {
+                    sftp.UploadFile(uplfileStream, fileName, true);
+                }
+                sftp.Disconnect();
+            }
+            return 0;
+        }
+
+    }
+}
